8cea82242326af29f2088ffe542b95d32534bfde
PlumRice <>
2024-10-14 09:38:40 +0900

    migrate更新

--- server/tasks/db/migrate.ts
+++ server/tasks/db/migrate.ts
@@ -70,7 +70,7 @@ async function checkMemberDB(db: Database): Promise<{
     }[] = [];
     for (const member of ConfigMembers) {
         const DbHandle = await selectYoutubeHandleQuery.get(member.name);
-        if (!(DbHandle === undefined && DbHandle["youtube_handle"] !== member.youtubeHandle)) {
+        if (!(DbHandle === undefined && DbHandle["youtube_handle"] === undefined && DbHandle["youtube_handle"] !== member.youtubeHandle)) {
             console.log("diff not change", DbHandle["youtube_handle"], member.youtubeHandle)
             continue;
         }
